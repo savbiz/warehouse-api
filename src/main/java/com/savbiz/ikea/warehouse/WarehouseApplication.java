1d13f113fd61cbcda1651032083c19e97a6a08ab
Savino Bizzoca <sav.bizzoca@gmail.com>
2021-02-08 11:12:10 +0100

    First commit

---
+++ src/main/java/com/savbiz/ikea/warehouse/WarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.savbiz.ikea.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseApplication.class, args);
+	}
+
+}
